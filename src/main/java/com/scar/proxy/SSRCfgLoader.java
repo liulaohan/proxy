ca073ab2de06f85b2a16496eeee745bbfadf14fc
scar <scar@scardeiMac.local>
2018-12-29 17:29:22 +0800

    1

--- src/main/java/com/scar/proxy/SSRCfgLoader.java
+++ src/main/java/com/scar/proxy/SSRCfgLoader.java
@@ -7,21 +7,22 @@ import com.google.zxing.common.HybridBinarizer;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
 import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @description
@@ -39,9 +40,12 @@ public class SSRCfgLoader {
 
     private Proxy proxy = new Proxy(java.net.Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 1080));
 
-    public String loadSsFree() throws IOException {
-        Connection.Response response = Jsoup.connect(URL_SS_FREE).method(Connection.Method.GET).execute();
-        List<Element> elements = response.parse().getElementsByTag("code");
+    public Map<String, Object> loadSsFreeSSL() throws IOException {
+        Map<String, Object> map = new HashMap<>();
+        Connection.Response response = Jsoup.connect(URL_SS_FREE).proxy(proxy).method(Connection.Method.GET).execute();
+        Document document = response.parse();
+        System.out.println(document);
+        List<Element> elements = document.getElementsByTag("code");
         List<Config> configs = new ArrayList<>();
         elements.forEach(x -> {
             if(x.text().startsWith("ip")) {
@@ -53,33 +57,28 @@ public class SSRCfgLoader {
                 configs.add(cfg);
             }
         });
-
-        return JSONObject.toJSONString(configs);
+        map.put("config", configs);
+        return map;
     }
 
-    public String loadFreeSsSSL() throws Exception {
+    public Map<String, Object> loadFreeSsSSL() throws Exception {
+        Map<String, Object> map = new HashMap<>();
         Connection.Response response = Jsoup.connect(URL_FREE_SS).method(Connection.Method.GET).proxy(proxy).execute();
         List<Element> elements = response.parse().getElementsByAttributeValueMatching("href", "^data:image");
+        List<String> ssList = new ArrayList<>();
         elements.forEach(x -> {
             try {
                 String target = StringUtils.substringAfter(x.attr("href").toString(), "data:image/png;base64,");
                 byte[] data = new BASE64Decoder().decodeBuffer(target);
                 ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
-                BufferedImage bufferedImage = ImageIO.read(byteArrayInputStream);
-                LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
-                Binarizer binarizer = new HybridBinarizer(source);
-                BinaryBitmap binaryBitmap = new BinaryBitmap(binarizer);
-                Map<DecodeHintType, Object> hints = new HashMap<DecodeHintType, Object>();
-                hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
-                Result result = new MultiFormatReader().decode(binaryBitmap, hints);
-                System.out.println(result.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (NotFoundException e) {
+                String result = img2QRCodeString(byteArrayInputStream);
+                ssList.add(result);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         });
-        return "";
+        map.put("ss", ssList);
+        return map;
     }
 
     public String loadFreeSsSiteSSL() throws IOException {
@@ -88,34 +87,143 @@ public class SSRCfgLoader {
         return "";
     }
 
-    public String loadIShadowXSSL() throws IOException {
+    public Map<String, Object> loadIShadowXSSL() throws IOException {
+        Map<String, Object> map = new HashMap<>();
         Connection.Response response = Jsoup.connect(URL_ISHADOWX).method(Connection.Method.GET).proxy(proxy).execute();
         List<Element> elements = response.parse().getElementsByClass("hover-text");
+        List<Config> configs = new ArrayList<>();
+        List<String> ssList = new ArrayList<>();
         elements.forEach(x -> {
-            x.getElementsByTag("h4").forEach(n -> {
+            String ip = "";
+            String port = "";
+            String password = "";
+            String method = "";
+            for(Element n: x.getElementsByTag("h4")) {
+                // System.out.println(n.toString());
                 if(n.text().contains("IP")) {
-                    System.out.println(n.getElementsByTag("span").get(0).text());
+                    ip = n.getElementsByTag("span").get(0).text();
                 } else if(n.text().contains("Port")) {
-                    System.out.println(n.getElementsByTag("span").get(0).text());
+                    port = n.getElementsByTag("span").get(0).text();
                 } else if(n.text().contains("Password")) {
-                    System.out.println(n.getElementsByTag("span").get(0).text());
+                    password = n.getElementsByTag("span").get(0).text();
                 } else if(n.text().contains("Method")) {
-                    System.out.println(StringUtils.substringAfter(n.text(), ":"));
+                    method = StringUtils.substringAfter(n.text(), ":");
                 }
-            });
+            }
+            if(!"".equals(ip)) {
+                configs.add(new Config(ip, Long.parseLong(port), password, method));
+            }
             List<Element> nodes = x.getElementsByAttributeValueMatching("data-clipboard-text", "^vmess\\S+");
             if(nodes != null) {
                 nodes.forEach(y -> {
-                    System.out.println(y.attr("data-clipboard-text"));
+                    ssList.add(y.attr("data-clipboard-text"));
                 });
             }
         });
-        return "";
+        map.put("config", configs);
+        map.put("ss", ssList);
+        return map;
     }
 
-    public String loadSS8SSL() throws IOException {
+    public Map<String, Object> loadSS8SSL() throws IOException {
+        Map<String, Object> map = new HashMap<>();
         Connection.Response response = Jsoup.connect(URL_SS8).method(Connection.Method.GET).proxy(proxy).execute();
-        System.out.println(response.body());
-        return "";
+        List<String> ssList = new ArrayList<>();
+        response.parse().getElementsByClass("image").forEach(x -> {
+            try {
+                String url = x.attr("href");
+                Connection.Response r = Jsoup.connect(URL_SS8 + url).ignoreContentType(true).method(Connection.Method.GET).proxy(proxy).execute();
+                String result = img2QRCodeString(r.bodyStream());
+                ssList.add(result);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        map.put("ss", ssList);
+        return map;
+    }
+
+    public String img2QRCodeString(InputStream inputStream) throws Exception {
+        BufferedImage bufferedImage = ImageIO.read(inputStream);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        Binarizer binarizer = new HybridBinarizer(source);
+        BinaryBitmap binaryBitmap = new BinaryBitmap(binarizer);
+        Map<DecodeHintType, Object> hints = new HashMap<DecodeHintType, Object>();
+        hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+        Result result = new MultiFormatReader().decode(binaryBitmap, hints);
+        return result.toString();
+    }
+    public String img2Base64String(String url) throws IOException {
+        Connection.Response response = Jsoup.connect(url).method(Connection.Method.GET).proxy(proxy).execute();
+        InputStream inputStream = response.bodyStream();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] data = new byte[1024];
+        int len = -1;
+        while ((len = inputStream.read(data)) != -1) {
+            outputStream.write(data, 0, len);
+        }
+        inputStream.close();
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(outputStream.toByteArray());
+    }
+
+    public Map<String, String> readRepertory() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        Scanner scanner = new Scanner(new File("src/main/resources/data.properties"));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] data = line.split("=");
+            if(data.length == 2) {
+                map.put(data[0].trim(), data[1].trim());
+            }
+        }
+        return map;
+    }
+
+    public void addRepertory(List<String> data) throws Exception {
+        FileWriter writer = new FileWriter("src/main/resources/data.properties", false);
+        PrintWriter printWriter = new PrintWriter(writer);
+        data.forEach(x -> {
+            printWriter.println(x);
+        });
+        writer.flush();
+        printWriter.close();
+        writer.close();
+    }
+
+    public List<Config> loadStringSsrSSL(String url) throws Exception {
+        Connection.Response response = Jsoup.connect(url).proxy(proxy).method(Connection.Method.GET).execute();
+        String html = response.parse().toString();
+        Pattern pattern = Pattern.compile("ss(r)?://[a-zA-Z0-9+/=]+");
+        Matcher matcher = pattern.matcher(html);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+        return null;
+    }
+
+    public void decodeBase64SsrString(String url) {
+        url = url.replaceAll("ss(r)?://", "");
+        String result = new String(Base64Utils.decodeFromUrlSafeString(url));
+        JSONObject object = new JSONObject();
+        String paramStr = StringUtils.substringAfter(result, "/?");
+        if(!"".equals(paramStr)) {
+            String[] params = paramStr.split("&");
+            for(String s: params) {
+                String key = s.split("=")[0];
+                String value = s.split("=")[1];
+                object.put(key, value);
+            }
+        }
+        String cfgString = StringUtils.substringBefore(result, "/?");
+        String[] values = cfgString.split(":");
+        //host, port, protocol, method, obfs
+        object.put("host", values[0]);
+        object.put("port", Long.parseLong(values[1]));
+        object.put("protocol", values[2]);
+        object.put("method", values[3]);
+        object.put("obfs", values[4]);
+        object.put("password", new String(Base64Utils.decodeFromUrlSafeString(values[5])));
+        System.out.println(object.toJSONString());
     }
 }
--- src/test/java/com/scar/proxy/SSRCfgLoaderApplicationTests.java
+++ src/test/java/com/scar/proxy/SSRCfgLoaderApplicationTests.java
@@ -1,10 +1,19 @@
 package com.scar.proxy;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Base64Utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,12 +21,38 @@ public class SSRCfgLoaderApplicationTests {
 	@Autowired
 	private SSRCfgLoader sSRCfgLoader;
 	@Test
-	public void contextLoads() {
-		try {
-			System.out.println(sSRCfgLoader.loadSS8SSL());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public void contextLoads() throws Exception {
+		List<String> ssList = new ArrayList<>();
+		List<Config> configs = new ArrayList<>();
+		merge(ssList, configs, sSRCfgLoader.loadFreeSsSSL(), sSRCfgLoader.loadIShadowXSSL(), sSRCfgLoader.loadSS8SSL(), sSRCfgLoader.loadSsFreeSSL());
+
+		configs.forEach(x -> {
+			System.out.println(JSONObject.toJSONString(x));
+		});
 	}
 
+
+	@Test
+	public void addFile() throws Exception {
+		sSRCfgLoader.decodeBase64SsrString("MTg0LjE3Mi4yMzMuMTU4OjMwMDI0OmF1dGhfY2hhaW5fYTpub25lOnBsYWluOk1USXpabkpsWldOc2IzVmtMblJyLz9yZW1hcmtzPTU3Nk81WnU5NUx5UjVwYXY2YUdfJmdyb3VwPVUxTlNVMGhCVWtVdVEwOU4mdWRwcG9ydD0yJnVvdD0x");
+	}
+
+	public void merge(List<String> ssList, List<Config> configs, Map<String, Object> ...maps) {
+		for(Map<String, Object> map: maps) {
+			if(map.containsKey("ss")) {
+				ssList.addAll((Collection<? extends String>) map.get("ss"));
+			}
+			if(map.containsKey("config")) {
+				configs.addAll((Collection<? extends Config>) map.get("config"));
+			}
+		}
+		for(String s: ssList) {
+			System.out.println(s);
+//			if(s.startsWith("ss://")) {
+//				System.out.println(Base64Utils.decodeFromString(StringUtils.substringAfter(s, "ss://")));
+//				String[] ss = new String(Base64Utils.decodeFromString(StringUtils.substringAfter(s, "ss://"))).split(":");
+//				Config config = new Config(ss[1].split("@")[1].trim(), Long.parseLong(ss[2].trim()), ss[1].split("@")[0].trim(), ss[0].trim());
+//			}
+		}
+	}
 }
